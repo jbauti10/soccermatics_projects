636f14ad5851f69ee44775135b337b33edac928e
Juan Sebastián Bautista Gamboa <67027970+jbauti10@users.noreply.github.com>
2024-06-10 14:50:52 -0500

    Added project folders

---
+++ Project_3_JuanBautista/settings.py
@@ -0,0 +1,19 @@
+# Path To Data
+DATA_FOLDER_FF = 'Data/freeze frames PL 2021/'
+
+DATA_FOLDER_PC = 'Data/possession chains PL 2021/'
+
+PROCESSED_FOLDER = 'Data/processed/'
+
+
+df_output_name = 'event_data'
+
+ff_output_name = 'freeze_frames'
+
+pattern_match = "match:{}:event:{}"
+
+prefix = '_tracking_data_ff.json'
+
+pitch_dimension = (105, 68)
+
+df_output_name_pass = 'event_data_pass'
